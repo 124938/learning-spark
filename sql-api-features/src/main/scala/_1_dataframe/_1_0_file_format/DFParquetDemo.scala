e01264341afbe60a4d47a0d3187e47798f5f9413
124938 <shrey.9129@gmail.com>
2017-11-05 21:01:40 +0530

    DataFrame Parquet demo

---
+++ sql-api-features/src/main/scala/_1_dataframe/_1_0_file_format/DFParquetDemo.scala
@@ -0,0 +1,88 @@
+package _1_dataframe._1_0_file_format
+
+import org.apache.spark.sql.{SQLContext, SaveMode}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object DFParquetDemo {
+  def main(args: Array[String]): Unit = {
+
+    // Create Spark Configuration
+    val conf = new SparkConf().
+      setAppName("DF Parquet File - Demo").
+      setMaster("local[2]")
+
+    // Create Spark Context
+    val sc = new SparkContext(conf)
+
+    // Create SQL Context
+    val sqlContext = new SQLContext(sc)
+
+    // Below is used to create DataFrame from RDD
+    import sqlContext.implicits._
+
+    val orderTextFileLocation = "/home/asus/source_code/github/124938/learning-spark/sql-api-features/src/main/resources/retail_db/orders/text"
+
+    // Create DataFrame from text file
+    val orderDF = sc.
+      textFile(orderTextFileLocation).
+      map((rec: String) => {
+        val recArray = rec.split(",")
+        (recArray(0).toInt, recArray(1), recArray(2).toInt, recArray(3))
+      }).
+      toDF("order_id", "order_date", "order_customer_id", "order_status")
+
+    val orderParquetFileLocation = "/tmp/retail_db/orders/parquet"
+
+    println("**** Problem Statement : Write & Verify Parquet data without compression ****")
+
+    // Default compression codec of parquet file is gzip
+    sqlContext.setConf("spark.sql.parquet.compression.codec", "uncompressed")
+
+    orderDF.
+      write.
+      mode(SaveMode.Overwrite).
+      parquet(orderParquetFileLocation)
+
+    // Verify Parquet files
+    sqlContext.
+      read.
+      parquet(orderParquetFileLocation).
+      show(20)
+
+    println("**** Problem Statement : Write & Verify Parquet data with GZIP compression ****")
+
+    val orderParquetGzipFileLocation = orderParquetFileLocation + "_" + System.currentTimeMillis()
+
+    // Default compression codec of parquet file is gzip
+    sqlContext.setConf("spark.sql.parquet.compression.codec", "gzip")
+
+    orderDF.
+      write.
+      mode(SaveMode.Overwrite).
+      parquet(orderParquetGzipFileLocation)
+
+    // Verify Parquet files
+    sqlContext.
+      read.
+      parquet(orderParquetGzipFileLocation).
+      show(20)
+
+    println("**** Problem Statement : Write & Verify Parquet data with Snappy compression ****")
+
+    val orderParquetSnappyFileLocation = orderParquetFileLocation + "_" + System.currentTimeMillis()
+
+    // Default compression codec of parquet file is snappy
+    sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")
+
+    orderDF.
+      write.
+      mode(SaveMode.Overwrite).
+      parquet(orderParquetSnappyFileLocation)
+
+    // Verify Parquet files
+    sqlContext.
+      read.
+      parquet(orderParquetSnappyFileLocation).
+      show(20)
+  }
+}
