6cbe23e504103312c49b3157fb721c42c602e114
Shreyashkumar H. Limbhetwala <shrey.9129@gmail.com>
2018-05-20 16:07:59 +0530

    Netcat word count program

---
+++ streaming-api-features/src/main/scala/NetcatWordCount.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object NetcatWordCount {
+
+  def main(args: Array[String]): Unit = {
+    // Get arguments from program
+    val mode = args(0)
+    val host = args(1)
+    val port = args(2).toInt
+
+    // Create Spark Config
+    val conf = new SparkConf().
+      setMaster(mode).
+      setAppName("Network word count")
+
+    // Create Spark Context
+    val sc = new SparkContext(conf)
+
+    // Create Spark Streaming Context
+    val ssc = new StreamingContext(sc, Seconds(5))
+
+    // Create word count program
+    val lines = ssc.socketTextStream(host, port)
+    val words = lines.flatMap((line: String) => line.split(" "))
+    val wordsMap = words.map((word: String) => (word, 1))
+    val wordCount = wordsMap.reduceByKey((agg: Int, ele: Int) => agg + ele)
+
+    // Print output
+    wordCount.print
+
+    // Start spark streaming
+    ssc.start
+    ssc.awaitTermination
+  }
+
+}
